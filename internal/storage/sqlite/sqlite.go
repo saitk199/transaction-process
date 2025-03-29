7f937a077f676f8be0663295f8d0d94bb7665ee2
PetrOstrovskiy <118372350+PetrOstrovskiy@users.noreply.github.com>
2025-03-29 22:24:09 +0700

    Fix sqlite

--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -14,33 +14,35 @@ type Storage struct {
 func New(storagePath string) (*Storage, error) {
 	const op = "storage.sqlite.New"
 
-	db, err := sql.Open("sqlite", storagePath)
+	db, err := sql.Open("sqlite", storagePath+"?_pragma=journal_mode=WAL&_pragma=cache_size=10000&_pragma=synchronous=NORMAL")
 	if err != nil {
-		return nil, fmt.Errorf("%s: $w", op, err)
+		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
 	stmt, err := db.Prepare(`
 	CREATE TABLE IF NOT EXISTS vallet(
-		adress TEXT PRIMARY KEY,
-		balance REAL NOT NULL
-		);
+    	adress TEXT PRIMARY KEY,
+    	balance REAL NOT NULL
+	);
+
 	CREATE TABLE IF NOT EXISTS transaction(
-		id TEXT PRIMARY KEY,
-		from TEXT NOT NULL,
-		FOREIGN KEY(from) REFERENCES vallet(adress),
-		to TEXT NOT NULL,
-		FOREIGN KEY(to) REFERENCES vallet(adress),
-		date INTEGER
+    	id TEXT PRIMARY KEY,
+    	from TEXT NOT NULL,
+    	to TEXT NOT NULL,
+    	date INTEGER,
+    	FOREIGN KEY(from) REFERENCES vallet(adress),
+    	FOREIGN KEY(to) REFERENCES vallet(adress)
+	);
+
 	CREATE INDEX IF NOT EXISTS idx_adress ON vallet(adress);
-	)
 	`)
 	if err != nil {
-		return nil, fmt.Errorf("%s: $w", op, err)
+		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
 	_, err = stmt.Exec()
 	if err != nil {
-		return nil, fmt.Errorf("%s: $w", op, err)
+		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
 	return &Storage{db: db}, nil
